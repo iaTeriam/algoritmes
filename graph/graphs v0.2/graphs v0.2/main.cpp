a49277171c20c32d1fc19125ef0ab2ff018e732d
Ilya <iaTeriam@mail.ru>
2019-05-11 13:10:38 +0500

    Добавил алгоритма Дейкстра в "поиск крайтчайшего пути в графе"

--- graph/graphs v0.2/graphs v0.2/main.cpp
+++ graph/graphs v0.2/graphs v0.2/main.cpp
@@ -1,12 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
 #include <stdio.h> 
 #include <conio.h> 
 #include <random> 
 #include <locale> 
 
+using namespace std;
+
 int graph_start = 0; //от какого графа
 int graph_end = 0; //до какого графа
-int graph[7][7]; // граф
+int GR[7][7]; // граф
+const int SIZE = 7;
+int st;
+
 
 int input() {
 	//printf("\nЗаполнение началось\n");
@@ -15,98 +21,64 @@ int input() {
 		for (int j = 0; j < 7; j++) {
 			//printf("\n Second Цикл %d\n", i);
 			if (j < i) {
-				graph[i][j] = graph[j][i];
+				GR[i][j] = GR[j][i];
 				//printf("\nПоложение_1\n"); //для отслеживания 
 			}
 			else if (j == i) {
-				graph[i][j] = 0;
+				GR[i][j] = 0;
 				//printf("\nПоложение_2\n");//для отслеживания
 			}
 			else {
-				graph[i][j] = 1 + rand() % 10;
+				GR[i][j] = 1 + rand() % 10;
 				//printf("\nПоложение_3\n");//для отслеживания
 			}
 		}
 	}
+	printf("Начальная вершина:");
+	scanf("%d", &st);
+	st--;
 	return 0;
 }
 
-int operation() {
-	printf("\nВведите начальный граф\n");
-	scanf("%d", &graph_start);
-	printf("\nВведите конечный граф\n");
-	scanf("%d", &graph_end);
-
-	//алгоритм дейкстры 
-
-	int mark[7];
-	int no_mark[7];
-
-	int s[7]; //первый массив из алгоритма дейкстры (помещаем переменные) 
-	for (int i = 0; i <= 6; i++) { //зануление массива 
-		s[i] = 0;
+const int V = 7;
+//алгоритм Дейкстры
+void algoritm()
+{
+	int distance[V], count, index, i, u, m = st + 1;
+	bool visited[V];
+	for (i = 0; i < V; i++)
+	{
+		distance[i] = INT_MAX; visited[i] = false;
 	}
-
-	int d[7];  //второй массив из аглоритма дейкстра (изменияем переменные)
-
-	int min = 1000;
-	int id_min = 0;
-
-	s[0] = graph_start;
-	printf("\nначало алгоритма\n");
-	printf("\nшаг первый\n");
-	for (int i = 0; i <= 6; i++) {
-		if (i == graph_start) {
-			d[i] = 0;
-		}
-		else {
-			d[i] = graph[graph_start][i];
-		}
-		if (d[i] > 0 && d[i] < min) {
-			min = d[i];
-			id_min = i;
-		}
-		printf("\nd[i] = %d\t", d[i]);
-		printf("минимум:%d\n", min);
+	distance[st] = 0;
+	for (count = 0; count < V - 1; count++)
+	{
+		int min = INT_MAX;
+		for (i = 0; i < V; i++)
+			if (!visited[i] && distance[i] <= min)
+			{
+				min = distance[i]; index = i;
+			}
+		u = index;
+		visited[u] = true;
+		for (i = 0; i < V; i++)
+			if (!visited[i] && GR[u][i] && distance[u] != INT_MAX &&
+				distance[u] + GR[u][i] < distance[i])
+				distance[i] = distance[u] + GR[u][i];
 	}
-	printf("\n Конец шаг первый\n");
-
-	//доделать шаг 2
-
-
-
-
-
-	return 0;
+	printf("Стоимость пути из начальной вершины до остальных:\t\n");
+	for (i = 0; i < V; i++) if (distance[i] != INT_MAX)
+		printf("%d > %d = %d\n", m, i + 1, distance[i]);
+	else printf("%d > %d = маршрут недоступен\n", m, i + 1);
 }
-
-/*int proverka(int d[], int graph_start, int id_min) {
-	int min = 1000;
-	for (int i = 0; i <= 6; i++) {
-		if (i == graph_start) {
-			d[i] = 1000;
-		}
-		else {
-			d[i] = graph[graph_start][i];
-		}
-		if (d[i] > 0 & d[i] < min) {
-			min = d[i];
-			id_min = i;
-		}
-		printf("\nd[i] = %d\t", d[i]);
-		printf("минимум:%d\n", min);
-	}
-}*/
 
 int show() {
 	for (int i = 0; i < 7; i++) {
 		printf("\n");
 		for (int j = 0; j < 7; j++) {
-			printf("%d\t", graph[i][j]);
+			printf("%d\t", GR[i][j]);
 		}
 	}
-	printf("\nОт графа %d, до графа %d, минимальное растояние = \n", graph_start, graph_end);
-	//вывести путь
 
 	return 0;
 }
@@ -119,7 +91,7 @@ int main() {
 
 
 	input();
-	operation();
+	algoritm();
 	show();
 
 	_getch();
