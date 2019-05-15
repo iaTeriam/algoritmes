b4b6a1448d3ec68217691cc9cef2b9796b7676be
Ilya <iaTeriam@mail.ru>
2019-05-16 00:31:18 +0500

    Добавил вторую версию рюкзака и доделал графы

--- backpack/bakcpack v2/backpack v2/backpack v2/main.cpp
+++ backpack/bakcpack v2/backpack v2/backpack v2/main.cpp
@@ -1,34 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <conio.h>
-#include <locale>
-//const int kvar = 300;
-//int R = 31;
-//int a[] = { 1,2,6,9,13,14,15,16,17,18,19,20,21,22,23,24,7,8,10,11,12,3,4,5 };
+#include "stdio.h"
+#include "locale.h"
+#include "conio.h"
+
+
 int R = 15;
-int a[] = { 7,8,10,11,12,13,3,4,5 };
+int a[] = {13,14,15,1,2,6,7, 10, 8, 11, 12, 3, 4, 5 };
 const int n = sizeof(a) / sizeof(int);
-int *v = new int[n];
+int v[n];
 int best[100][n];
-
 int kbest = 0;
-int max = 0;
-int sum = 0;
-
-
-
-int next() {
-	for (int i = 0; i < n; ++i) {
-		if (v[i] == 1)
-			v[i] = 0;
-		else {
-			v[i] = 1;
-			return 1;
-		}
-		return 0;
-	}
-	return 0;
-}
+int mx = 0;
 
 void mark() {
 	int sum = 0;
@@ -37,41 +19,44 @@ void mark() {
 			sum += a[i];
 	}
 	if (sum > R) return;
-	if (sum < max)return;
-	if (sum > max) {
-		kbest = 0;
-		max = sum;
-	}
-	if (kbest < 100) {
+	if (sum < mx) return;
+	if (sum > mx) { kbest = 0; mx = sum; }
+	if (kbest < 100)
 		for (int i = 0; i < n; ++i)
 			best[kbest][i] = v[i];
-	}
 	kbest++;
 }
 
-void show() {
-	for (int i = 0; i < (kbest < 100 ? kbest : 100); ++i) {
-		for (int j = 0; j < n; ++j)
-			if (best[i][j])
-				printf("%3i", a[j]);
-		printf("\n");
-	}
-	printf("kbest = %d\n", kbest);
-	printf("R = %d", R);
+int next() {
+	for (int i = 0; i < n; ++i)
+		if (v[i] == 1)v[i] = 0;
+		else { v[i] = 1; return 1; }
+	return 0;
 }
 
+void show() {
 
+	for (int j = 0; j < (kbest < 100 ? kbest : 100); ++j) {
+		printf("%d лучшая выборка\n", j + 1);
+		for (int i = 0; i < n; ++i) {
+			if (best[j][i] == 1)
+				printf(" a[%d]=%d, ", i, a[i]);
+		}
+		printf("\n\n");
+	}
+	printf("kbest=%i\n", kbest);
+}
 
 int main() {
 	setlocale(LC_ALL, "rus");
 
-	for (int i = 0; i < n; ++i) {
+	for (int i = 0; i < n; ++i)
 		v[i] = 0;
-		v[n - 1] = 1;
-		do {
-			mark();
-		} while (next());
-	}
+	v[0] = 1;
+	do {
+		mark();
+	} while (next());
+
 	show();
 	_getch();
 	return 0;
--- graph/graphs v0.2/graphs v0.2/main.cpp
+++ graph/graphs v0.2/graphs v0.2/main.cpp
@@ -1,86 +1,92 @@
 #define _CRT_SECURE_NO_WARNINGS 
-#include <iostream>
 #include <stdio.h> 
 #include <conio.h> 
 #include <random> 
 #include <locale> 
 
-using namespace std;
-
-int graph_start = 0; //от какого графа
+int start = 0; //от какого графа
 int graph_end = 0; //до какого графа
-int GR[7][7]; // граф
-const int SIZE = 7;
-int st;
+const int V = 7;
+int GR[V][V]; // граф
 
 
-int input() {
-	//printf("\nЗаполнение началось\n");
-	for (int i = 0; i < 7; i++) {
-		//printf("\n First Цикл %d\n",i);
-		for (int j = 0; j < 7; j++) {
-			//printf("\n Second Цикл %d\n", i);
+void input() {
+	for (int i = 0; i < V; i++) {
+		for (int j = 0; j <V ; j++) {
 			if (j < i) {
 				GR[i][j] = GR[j][i];
-				//printf("\nПоложение_1\n"); //для отслеживания 
 			}
 			else if (j == i) {
 				GR[i][j] = 0;
-				//printf("\nПоложение_2\n");//для отслеживания
 			}
 			else {
-				GR[i][j] = 1 + rand() % 10;
-				//printf("\nПоложение_3\n");//для отслеживания
+				printf("Чему равен путь из %d в %d:\n",i+1,j+1);
+				scanf("%d", &GR[i][j]);
+				//GR[i][j] = 0 + rand() % 10;
 			}
 		}
 	}
 	printf("Начальная вершина:");
-	scanf("%d", &st);
-	st--;
-	return 0;
+	scanf("%d", &start);
+
+	printf("Конечная вершина:");
+	scanf("%d", &graph_end);
+
+	start--;
+	graph_end--;
 }
 
-const int V = 7;
 //алгоритм Дейкстры
 void algoritm()
 {
-	int distance[V], count, index, i, u, m = st + 1;
+	int distance[V], count, index, i, u, m = start + 1;
 	bool visited[V];
+	int test1, test2;
+
+
 	for (i = 0; i < V; i++)
 	{
 		distance[i] = INT_MAX; visited[i] = false;
 	}
-	distance[st] = 0;
-	for (count = 0; count < V - 1; count++)
-	{
+
+	distance[start] = 0;
+	printf("Путь где номера это вершины:\n");
+	for (count = 0; count < V - 1; count++){
 		int min = INT_MAX;
 		for (i = 0; i < V; i++)
-			if (!visited[i] && distance[i] <= min)
+			if (!visited[i] && distance[i] <= min) 
 			{
-				min = distance[i]; index = i;
+				min = distance[i]; 
+				index = i;
 			}
 		u = index;
 		visited[u] = true;
-		for (i = 0; i < V; i++)
-			if (!visited[i] && GR[u][i] && distance[u] != INT_MAX &&
-				distance[u] + GR[u][i] < distance[i])
+		
+		for (i = 0; i < V; i++) {
+			test1 = distance[graph_end];
+			if (!visited[i] && GR[u][i] && distance[u] != INT_MAX && distance[u] + GR[u][i] < distance[i])
 				distance[i] = distance[u] + GR[u][i];
+			test2 = distance[graph_end];
+			if (test1 != test2) {
+			printf("%d\t", u+1);
+			}
+		}
 	}
+	printf("%d", graph_end+1);
+	printf("\n");
 	printf("Стоимость пути из начальной вершины до остальных:\t\n");
 	for (i = 0; i < V; i++) if (distance[i] != INT_MAX)
 		printf("%d > %d = %d\n", m, i + 1, distance[i]);
 	else printf("%d > %d = маршрут недоступен\n", m, i + 1);
 }
 
-int show() {
+void show() {
 	for (int i = 0; i < 7; i++) {
 		printf("\n");
 		for (int j = 0; j < 7; j++) {
 			printf("%d\t", GR[i][j]);
 		}
 	}
-
-	return 0;
 }
 
 int main() {
